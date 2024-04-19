8f5dc67813a9b4bc0d65e3a919315eccf3f1c92f
m-faried <m.a.faried@gmail.com>
2024-04-19 18:28:26 +0200

    adding the handler and the main

--- api/main.go
+++ api/main.go
@@ -1,1 +1,43 @@
 package api
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+
+	contract "api/contract"
+)
+
+const (
+	NodeURL         = ""
+	ContractAddress = ""
+	ChainID         = 0
+)
+
+func Main() {
+	r := gin.Default()
+
+	conn, err := ethclient.Dial(NodeURL)
+	if err != nil {
+		log.Err(err).Msg("Fail to initialise the eth client")
+		return
+	}
+
+	auction, err := contract.NewAuction(common.HexToAddress(ContractAddress), conn)
+	if err != nil {
+		log.Err(err).Msg("Fail to initialise the contract")
+		return
+	}
+
+	h := NewHandler(auction)
+
+	r.POST("/auction/start", h.startAuction)
+	r.POST("/auction/end", h.endAuction)
+	r.POST("/auction/withdraw", h.withdraw)
+	r.POST("/bids", h.createBid)
+
+	if err := r.Run(); err != nil {
+		log.Err(err).Msg("Server exited")
+	}
+}
